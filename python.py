f8e32d3add9acba7740db20f339ece28d5aeb458
SwNoppadon <sw.noppadon@gmail.com>
2022-01-19 23:18:07 +0700

    .

--- python.py
+++ python.py
@@ -1,5 +1,8 @@
 FROM flask import Flask
 app = Flask(__name__)
     @app.route('/')
-        def hello-world():
+        def new_func():
+    return def     hello-world():
+
+new_func()
             return 'Hello,Docker'
